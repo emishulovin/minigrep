842f9269bc3e82b9d98383736d2b0af10e728150
Eliezer Mishulovin <50752841+emishulovin@users.noreply.github.com>
2021-02-23 13:39:02 -0600

    split and moved general logic to lib file

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
 use std::env;
-use std::fs;
-use std:: process;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args)
-                .unwrap_or_else(|err| {
-                    println!("Problem passing the arguments: {}", err);
-                    process::exit(1);
-                });
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing the arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Failed to read file.");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Needs more arguments: a string to search for and a filename to search in.");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
